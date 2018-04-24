b9bca285ac142b940953a63d800664801721c005
RM <RM@DESKTOP-9QSUOKF.Home>
2018-04-24 11:24:49 +0200

    First Commit

--- src/snake/Board.java
+++ src/snake/Board.java
@@ -16,33 +16,34 @@ import javax.swing.Timer;
  *
  * @author alu26600537w
  */
-public class Board extends JPanel implements ActionListener{
-    
+public class Board extends JPanel implements ActionListener {
+
     MyKeyAdapter keyAdapter;
-    
+
     public ScoreBoard scoreBoard;
-    
+
     private int deltaTime;
-    
+    private DirectionType direction;
+
     private Food food;
     private SpecialFood specialFood;
     private Snake snake;
     private Timer timer;
-    
+
     private boolean gameOver;
-    
+
     public Board() {
         super();
         timer = new Timer(deltaTime, this);
         keyAdapter = new MyKeyAdapter();
     }
-    
+
     public void initValues() {
         setFocusable(true);
         deltaTime = 500;
         gameOver = false;
     }
-    
+
     public void initGame() {
         removeKeyListener(keyAdapter);
         initValues();
@@ -56,32 +57,40 @@ public class Board extends JPanel implements ActionListener{
     public void actionPerformed(ActionEvent ae) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-     class MyKeyAdapter extends KeyAdapter {
+
+    class MyKeyAdapter extends KeyAdapter {
 
         @Override
         public void keyPressed(KeyEvent e) {
+
             switch (e.getKeyCode()) {
-                case KeyEvent.VK_LEFT:
 
-                    break;
-                case KeyEvent.VK_RIGHT:
-
-                    break;
                 case KeyEvent.VK_UP:
-
+                    if (direction != DirectionType.down) {
+                        direction = DirectionType.up;
+                    }
                     break;
+
+                case KeyEvent.VK_LEFT:
+                    if (direction != DirectionType.right) {
+                        direction = DirectionType.left;
+                    }
+                    break;
+
+                case KeyEvent.VK_RIGHT:
+                    if (direction != DirectionType.left) {
+                        direction = DirectionType.right;
+                    }
+                    break;
+
                 case KeyEvent.VK_DOWN:
-
-                    break;
-                case KeyEvent.VK_P:
-
-                    break;
-
-                default:
+                    if (direction != DirectionType.up) {
+                        direction = DirectionType.down;
+                    }
                     break;
             }
+            repaint();
         }
     }
-    
+
 }
--- src/snake/Food.java
+++ src/snake/Food.java
@@ -10,5 +10,23 @@ package snake;
  * @author alu26600537w
  */
 public class Food {
+
+    private int foodX; // Stores X pos of our food
+    private int foodY; // Stores Y pos of our food
     private Node node;
+    
+    public void createFood() {
+
+    // Set our food's x & y position to a random position
+
+    int location = (int) (Math.random() * RANDOMPOSITION);
+    foodX = ((location * Board.getDotSize()));
+
+    location = (int) (Math.random() * RANDOMPOSITION);
+    foodY = ((location * Board.getDotSize()));
+
+    if ((foodX == snake.getSnakeX(0)) && (foodY == snake.getSnakeY(0))) {
+        createFood();
+    }
+}
 }
